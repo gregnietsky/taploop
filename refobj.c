5a3d477407d799e401177b447b24f0834f16ac1a
Greg Nietsky <gregory@distrotech.co.za>
2012-04-20 20:36:26 +0000

    White space fixup in refobj
    git-svn-id: http://ns2/svnsrc/taploop/trunk@1208 a03fbada-158d-47ef-9f12-77e997805b15

--- refobj.c
+++ refobj.c
@@ -16,114 +16,113 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <stdlib.h>
 #include <pthread.h>
 #include <string.h>
 #include <jhash.h>
+#include "list.h"
 
-#define REFOBJ_MAGIC            0xdeadc0de
+#define REFOBJ_MAGIC		0xdeadc0de
 
 /* ref counted objects*/
 struct ref_obj {
-        int     magic;
-        int     cnt;
-        pthread_mutex_t         lock;
-        void *data;
+	int	magic;
+	int	cnt;
+	pthread_mutex_t	lock;
+	void *data;
 };
 
 /* bucket list obj*/
 struct blist_obj {
-        int     bucket;
-        int     hash;
-        struct  blist_obj *next;
-        struct  blist_obj *prev;
-        struct  ref_obj *data;
+	int	hash;
+	struct	blist_obj *next;
+	struct	blist_obj *prev;
+	struct	ref_obj *data;
 };
 
 /*bucket list to hold hashed objects in buckets*/
 struct bucket_list {
-        unsigned short  buckets;        /* number of buckets to create 2 ^ n masks hash*/
-        struct blist_obj *list;         /* array of blist_obj[buckets]*/
-        int     (*hash_func)(void *data);
+	unsigned short	buckets;		/* number of buckets to create 2 ^ n masks hash*/
+	struct		blist_obj *list;		/* array of blist_obj[buckets]*/
+	int		(*hash_func)(void *data);
 };
 
 void *objalloc(int size) {
-        struct ref_obj *ref;
-        size = size+32;
-        void *robj;
+	struct ref_obj *ref;
+	size = size+32;
+	void *robj;
 
-        if ((robj = malloc(size))) {
-                memset(robj, 0, size);
-                ref = (struct ref_obj*)robj;
-                pthread_mutex_init(&ref->lock, NULL);
-                ref->magic = REFOBJ_MAGIC;
-                ref->cnt++;
-                return robj + 32;
-        }
-        return NULL;
+	if ((robj = malloc(size))) {
+		memset(robj, 0, size);
+		ref = (struct ref_obj*)robj;
+		pthread_mutex_init(&ref->lock, NULL);
+		ref->magic = REFOBJ_MAGIC;
+		ref->cnt++;
+		return robj + 32;
+	}
+	return NULL;
 }
 
 int objref(void *data) {
-        int ret = 0;
+	int ret = 0;
 
-        if (!data) {
-                return ret;
-        }
+	if (!data) {
+		return ret;
+	}
 
-        struct ref_obj *ref = data - 32;
-        if ((ref->magic == REFOBJ_MAGIC) && (ref->cnt)) {
-                pthread_mutex_lock(&ref->lock);
-                ref->cnt++;
-                ret = ref->cnt;
-                pthread_mutex_unlock(&ref->lock);
-        }
-        return ret;
+	struct ref_obj *ref = data - 32;
+	if ((ref->magic == REFOBJ_MAGIC) && (ref->cnt)) {
+		pthread_mutex_lock(&ref->lock);
+		ref->cnt++;
+		ret = ref->cnt;
+		pthread_mutex_unlock(&ref->lock);
+	}
+	return ret;
 }
 
 int objunref(void *data) {
-        int ret = 0;
+	int ret = 0;
 
-        if (!data) {
-                return ret;
-        }
+	if (!data) {
+		return ret;
+	}
 
-        struct ref_obj *ref = data - 32;
-        if ((ref->magic == REFOBJ_MAGIC) && (ref->cnt)) {
-                pthread_mutex_lock(&ref->lock);
-                ref->cnt--;
-                ret = ref->cnt;
-                pthread_mutex_unlock(&ref->lock);
-                if (!ret) {
-                        pthread_mutex_destroy(&ref->lock);
-                        free(ref);
-                }
-        }
-        return ret;
+	struct ref_obj *ref = data - 32;
+	if ((ref->magic == REFOBJ_MAGIC) && (ref->cnt)) {
+		pthread_mutex_lock(&ref->lock);
+		ref->cnt--;
+		ret = ref->cnt;
+		pthread_mutex_unlock(&ref->lock);
+		if (!ret) {
+			pthread_mutex_destroy(&ref->lock);
+			free(ref);
+		}
+	}
+	return ret;
 }
 
 int objcnt(void *data) {
-        int ret = -1;
-        struct ref_obj *ref = data - 32;
-        if (ref->magic == REFOBJ_MAGIC) {
-                pthread_mutex_lock(&ref->lock);
-                ret = ref->cnt;
-                pthread_mutex_unlock(&ref->lock);
-        }
-        return ret;
+	int ret = -1;
+	struct ref_obj *ref = data - 32;
+	if (ref->magic == REFOBJ_MAGIC) {
+		pthread_mutex_lock(&ref->lock);
+		ret = ref->cnt;
+		pthread_mutex_unlock(&ref->lock);
+	}
+	return ret;
 }
 
 void objlock(void *data) {
-        struct ref_obj *ref = data - 32;
+	struct ref_obj *ref = data - 32;
 
-        if (ref->magic == REFOBJ_MAGIC) {
-                pthread_mutex_lock(&ref->lock);
-        }
+	if (ref->magic == REFOBJ_MAGIC) {
+		pthread_mutex_lock(&ref->lock);
+	}
 }
 
 void objunlock(void *data) {
-        struct ref_obj *ref = data - 32;
+	struct ref_obj *ref = data - 32;
 
-        if (ref->magic == REFOBJ_MAGIC) {
-                pthread_mutex_unlock(&ref->lock);
-        }
+	if (ref->magic == REFOBJ_MAGIC) {
+		pthread_mutex_unlock(&ref->lock);
+	}
 }
