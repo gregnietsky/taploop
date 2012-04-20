df913cff6c44d9b48047dade51a53aaaa23bfd40
Greg Nietsky <gregory@distrotech.co.za>
2012-04-20 09:49:26 +0000

    Move thread funcs to thread.c / thread.h
    git-svn-id: http://ns2/svnsrc/taploop/trunk@1197 a03fbada-158d-47ef-9f12-77e997805b15

--- Makefile
+++ Makefile
@@ -16,7 +16,7 @@
 
 CFLAGS=-g -Wall -I./include
 
-TL_OBJS = taploop.o refobj.o util.o lookup3.o
+TL_OBJS = taploop.o refobj.o util.o lookup3.o thread.o
 
 all: taploop
 
--- taploop.c
+++ taploop.c
@@ -40,6 +40,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "refobj.h"
 #include "util.h"
+#include "thread.h"
 
 /* Use uthash lists
  * Copyright (c) 2007-2011, Troy D. Hanson   http://uthash.sourceforge.net All rights reserved.
@@ -55,16 +56,6 @@ enum sockopt {
 	TL_SOCKET_PHY	= 1 << 1,
 	/*when writing to this socket do so as 802.1q if vid set*/
 	TL_SOCKET_8021Q	= 1 << 2,
-};
-
-enum threadopt {
-	TL_THREAD_NONE	= 0,
-	/* thread is marked as running*/
-	TL_THREAD_RUN	= 1 << 1,
-	/* thread is marked as complete*/
-	TL_THREAD_DONE	= 1 << 2,
-	/* This is a taploop*/
-	TL_THREAD_TAP	= 1 << 3,
 };
 
 /* socket entry*/
@@ -86,22 +77,6 @@ struct taploop {
 	struct iovec *ring;	/*ring buffer phy*/
 	struct	tl_socket *socks;
 };
-
-/* thread struct used to create threads*/
-struct tl_thread {
-	void	*data;
-	enum threadopt flags;
-	void	*(*cleanup)(void *data);
-	pthread_t		thr;
-	struct tl_thread	*next;
-};
-
-/* thread list*/
-struct threadlist {
-	struct	tl_thread	*list;
-};
-
-struct threadlist *threads;
 
 /* tun/tap clone device and client socket*/
 char	*tundev = "/dev/net/tun";
@@ -766,118 +741,12 @@ void *mainloop(void *data) {
 }
 
 /*
- * this is run to flag running threads to stop and clean up dead threads
- */
-void checkthread(struct tl_thread *thread, int stop) {
-	objlock(thread);
-
-	if (stop && (thread->flags & TL_THREAD_RUN) && !(thread->flags & TL_THREAD_DONE)) {
-		thread->flags &= ~TL_THREAD_RUN;
-		objunlock(thread);
-		return;
-	}
-
-	if ((thread->flags & TL_THREAD_DONE) || pthread_kill(thread->thr, 0)){
-		LL_DELETE(threads->list, thread);
-		if  (thread->cleanup) {
-			thread->cleanup(thread->data);
-		}
-		objunlock(thread);
-		objunref(thread->data);
-		objunref(thread);
-		return;
-	}
-	objunlock(thread);
-}
-
-/*
- * loop through all threads till they stoped
- * setting stop will flag threads to stop
- */
-void verifythreads(int sl, int stop) {
-	struct tl_thread	*thread, *tmp;
-	pthread_t	me;
-
-	me =  pthread_self();
-	for(;;) {
-		objlock(threads);
-		if (!threads->list) {
-			objunlock(threads);
-			break;
-		}
-
-		LL_FOREACH_SAFE(threads->list , thread, tmp) {
-			checkthread(thread, stop);
-			/*this is my call im done*/
-			if ((pthread_equal(thread->thr, me)) &&
-			    (!(testflag(thread, &thread->flags, TL_THREAD_RUN)))) {
-				setflag(thread, &thread->flags, TL_THREAD_DONE);
-				pthread_cancel(me);
-				pthread_detach(me);
-				break;
-			}
-		}
-		objunlock(threads);
-
-		usleep(sl);
-	}
-}
-
-/*
  * handle signals to cleanup gracefully on exit
  */
 static void sig_handler(int sig, siginfo_t *si, void *unused) {
 	/* flag and clean all threads*/
 	verifythreads(10000, 1);
 	exit(0);
-}
-
-/*
- * create a taploop thread
- */
-struct tl_thread *mkthread(void *func, void *cleanup, void *data, enum threadopt flags) {
-	struct tl_thread *thread;
-
-	if (!(thread = objalloc(sizeof(*thread)))) {
-		return NULL;
-	}
-
-	thread->data = data;
-	thread->cleanup = cleanup;
-	thread->flags = 0;
-	thread->flags = flags;
-	/* set this and check this in thread*/
-	thread->flags &= ~TL_THREAD_RUN & ~TL_THREAD_DONE;
-
-	/* grab a ref to data for thread to make sure it does not go away*/
-	objref(thread->data);
-	if (pthread_create(&thread->thr, NULL, func, thread)) {
-		objunref(thread);
-		objunref(thread->data);
-		return NULL;
-	}
-
-	/* am i up and running move ref to list*/
-	if (!pthread_kill(thread->thr, 0)) {
-		objlock(threads);
-		LL_APPEND(threads->list, thread);
-		objunlock(threads);
-		return thread;
-	} else {
-		objunref(thread);
-	}
-
-	return NULL;
-}
-
-void *managethread(void *data) {
-	struct tl_thread *thread = data;
-
-	setflag(thread, &thread->flags, TL_THREAD_RUN);
-	verifythreads(100000, 0);
-	setflag(thread, &thread->flags, TL_THREAD_DONE);
-
-	return NULL;
 }
 
 /*
