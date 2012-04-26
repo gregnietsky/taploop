6cc5af47854f9854659c94755eccaab636f2de51
Greg Nietsky <gregory@distrotech.co.za>
2012-04-26 18:29:09 +0000

    Client protocol definition
    git-svn-id: http://ns2/svnsrc/taploop/trunk@1272 a03fbada-158d-47ef-9f12-77e997805b15

---
+++ src/include/client.h
@@ -0,0 +1,56 @@
+/*
+Copyright (C) 2012  Gregory Nietsky <gregory@distrotetch.co.za>
+        http://www.distrotech.co.za
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <sys/socket.h>
+#include <linux/if_arp.h>
+
+enum client_action {
+	CA_ADD		=	1 << 0,
+	CA_REM		=	1 << 1,
+};
+
+enum client_acttype {
+	CD_TAP		=	1 << 0,
+	CD_VLAN		=	1 << 1,
+};
+
+struct client_tap {
+	char			device[IFNAMSIZ+1];
+	char			name[IFNAMSIZ+1];
+};
+
+struct client_vlan {
+	char			device[IFNAMSIZ+1];
+	short int		vid;
+};
+
+union client_payload {
+	struct client_tap	tap;
+	struct client_vlan	vlan;
+};
+
+struct client_command {
+	enum	client_action	action;
+	enum	client_acttype	datatype;
+	union	client_payload	payload;
+};
+
+struct client_response {
+	int	error;
+	char	message[128];
+};
