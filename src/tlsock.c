8f017ec324fcc016a7649094d7e0f39b30cb46f4
Greg Nietsky <gregory@distrotech.co.za>
2012-05-15 10:22:48 +0000

    Unnneed headers removed from tlsock
    git-svn-id: http://ns2/svnsrc/taploop/trunk@1372 a03fbada-158d-47ef-9f12-77e997805b15

--- src/tlsock.c
+++ src/tlsock.c
@@ -18,12 +18,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <sys/ioctl.h>
 #include <sys/socket.h>
-#include <linux/if.h>
-#include <linux/if_arp.h>
 #include <linux/if_tun.h>
-#include <netinet/in.h>
 #include <string.h>
-#include <fcntl.h>
 #include <stdio.h>
 #include <errno.h>
 #include <unistd.h>
@@ -37,7 +33,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "taploop.h"
 #include "tlsock.h"
-#include "vlan.h"
 #include "packet.h"
 
 int hash_tapdata(void *data, int key) {
